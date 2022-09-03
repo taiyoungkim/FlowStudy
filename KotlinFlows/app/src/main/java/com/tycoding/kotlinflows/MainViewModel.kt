ec1ab84aeff7b561507d6f201bf32a4430de74c2
taiyoungkim <ktyoung153@gmail.com>
2022-09-03 16:22:37 +0900

    Flow Basic Study Day2

--- KotlinFlows/app/src/main/java/com/tycoding/kotlinflows/MainViewModel.kt
+++ KotlinFlows/app/src/main/java/com/tycoding/kotlinflows/MainViewModel.kt
@@ -3,9 +3,7 @@ package com.tycoding.kotlinflows
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
@@ -54,14 +52,14 @@ class MainViewModel : ViewModel() {
      * The current time is 0
      * */
 
-    private fun collectFlow() {
-        viewModelScope.launch {
-            countDownflow.collectLatest { time ->
-                delay(1500L)
-                println("The current time is $time")
-            }
-        }
-    }
+//    private fun collectFlow() {
+//        viewModelScope.launch {
+//            countDownflow.collectLatest { time ->
+//                delay(1500L)
+//                println("The current time is $time")
+//            }
+//        }
+//    }
     /**
      * The current time is 0
      *
@@ -71,4 +69,187 @@ class MainViewModel : ViewModel() {
      * 이전 데이터의 처리는 강제 종료 시키고
      * 새로운 데이터를 처리한다.
      * */
+
+//    private fun collectFlow() {
+//        viewModelScope.launch {
+//            val count = countDownflow
+//                .filter { time ->
+//                    time % 2 == 0
+//                }
+//                .map { time ->
+//                    time * time
+//                }
+//                .onEach { time ->
+//                    println(time)
+//                }
+//                .collect { time ->
+//                    println("The current time is $time")
+//                }
+//        }
+//    }
+
+    /**
+     * 중간 연산자!!
+     *
+     * filter는 filter에서 조건이 만족하는 값을 반환한다.
+     * map은 중간 연산자로 원하는 값으로 매핑하여 반환한다.
+     * onEach는 모든 데이터마다 연산 수행
+     */
+
+//    private fun collectFlow() {
+//        viewModelScope.launch {
+//            val reduceResult = countDownflow
+////                .reduce { accumulator, value ->
+////                    accumulator + value
+////                }
+//                .fold(100) { accumulator, value ->
+//                    accumulator + value
+//                }
+//            println("The current time is $reduceResult")
+//        }
+//    }
+
+    /**
+     * 종단 연산자!!
+     *
+     * reduce는 첫번째 원소부터 주어진 operation을 이용하여 누적시키면서 최종값을 반환한다.
+     * fold는 초기값 (위에서는 100)을 입력받아 reduce와 동인하게 작동한다.
+     *
+     */
+
+//    private fun collectFlow() {
+//        val flow1 = flow {
+//            emit(1)
+//            delay(500L)
+//            emit(2)
+//        }
+//        viewModelScope.launch {
+//            flow1.flatMapConcat { value ->
+//                flow {
+//                    emit(value  + 1)
+//                    delay(500L)
+//                    emit(value + 2)
+//                }
+//            }.collect { value ->
+//                println("The value is $value")
+//            }
+//        }
+//    }
+
+    /**
+     * The value is 2 (flow1에서 emit(1)이 반환되고 flow1 안에 flow에서 emit(value  + 1) 로 1+1이 되서 2가 출력
+     * The value is 3 (flow1안에 flow에 아직 있으므로 0.5초 후 flow1 안에 flow에서 emit(value  + 2) 로 1+2이 되서 3가 출력
+     * The value is 3 (flow1에서 emit(1)이 끝나고 0.5초 후 emit(2)가 반횐되고 flow1 안에 flow에서 다시 emit(value  + 1) 로 2+1이 되서 3가 출력
+     * The value is 4 (flow1안에 flow에 아직 있으므로 0.5초 후 flow1 안에 flow에서 emit(value  + 2) 로 2+2이 되서 4가 출력
+     */
+
+//    private fun collectFlow() {
+//        val flow1 = flow {
+//            delay(250L)
+//            emit("Appetizer")
+//            delay(1000L)
+//            emit("Main dish")
+//            delay(100L)
+//            emit("Desert")
+//        }
+//        viewModelScope.launch {
+//            flow1.onEach {
+//                println("FLOW: $it is delivered")
+//            }
+//                .collect {
+//                    println("FLOW: Now eating $it")
+//                    delay(1500L)
+//                    println("FLOW: Finished eating $it")
+//                }
+//        }
+//    }
+
+    /**
+     * FLOW: Appetizer is delivered
+     * FLOW: Now eating Appetizer
+     * FLOW: Finished eating Appetizer
+     * FLOW: Main dish is delivered
+     * FLOW: Now eating Main dish
+     * FLOW: Finished eating Main dish
+     * FLOW: Desert is delivered
+     * FLOW: Now eating Desert
+     * FLOW: Finished eating Desert
+     */
+
+//    private fun collectFlow() {
+//        val flow1 = flow {
+//            delay(250L)
+//            emit("Appetizer")
+//            delay(1000L)
+//            emit("Main dish")
+//            delay(100L)
+//            emit("Desert")
+//        }
+//        viewModelScope.launch {
+//            flow1.onEach {
+//                println("FLOW: $it is delivered")
+//            }
+//                .buffer()
+//                .collect {
+//                    println("FLOW: Now eating $it")
+//                    delay(1500L)
+//                    println("FLOW: Finished eating $it")
+//                }
+//        }
+//    }
+
+    /**
+     * buffer는 발행과 소비를 분리시키므로 순차적으로 기다리는게 아닌 바로바로 발행한다.
+     * FLOW: Appetizer is delivered
+     * FLOW: Now eating Appetizer
+     * FLOW: Main dish is delivered
+     * FLOW: Desert is delivered
+     * FLOW: Finished eating Appetizer
+     * FLOW: Now eating Main dish
+     * FLOW: Finished eating Main dish
+     * FLOW: Now eating Desert
+     * FLOW: Finished eating Desert
+     */
+
+    private fun collectFlow() {
+        val flow1 = flow {
+            delay(250L)
+            emit("Appetizer")
+            delay(1000L)
+            emit("Main dish")
+            delay(100L)
+            emit("Desert")
+        }
+        viewModelScope.launch {
+            flow1.onEach {
+                println("FLOW: $it is delivered")
+            }
+                .conflate()
+                .collectLatest {
+                    println("FLOW: Now eating $it")
+                    delay(1500L)
+                    println("FLOW: Finished eating $it")
+                }
+        }
+    }
+
+    /**
+     * conflate는 buffer와 같이 소비와 발행이 개별이지만 소비가 되기 전에 발행이 되면 해당 소비는 건너뛰고 발행을 한다.
+     * FLOW: Appetizer is delivered
+     * FLOW: Now eating Appetizer
+     * FLOW: Main dish is delivered
+     * FLOW: Desert is delivered
+     * FLOW: Finished eating Appetizer
+     * FLOW: Now eating Desert
+     * FLOW: Finished eating Desert
+     *
+     * 여기에 collectLatest를 사용하면
+     * FLOW: Appetizer is delivered
+     * FLOW: Now eating Appetizer
+     * FLOW: Main dish is delivered
+     * FLOW: Now eating Main dish
+     * FLOW: Desert is delivered
+     * FLOW: Now eating Desert
+     * FLOW: Finished eating Desert
+     */
 }
